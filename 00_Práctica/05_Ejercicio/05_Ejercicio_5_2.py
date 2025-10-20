457f6185823849074516f455497997ed176a4536
kacper koper <kacperkoper43@gmail.com>
2025-10-20 18:16:04 +0100

    Delete 00_Práctica directory

--- 00_Práctica/05_Ejercicio/05_Ejercicio_5_2.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-m1 = hashlib.sha512() #A nivel de criptografia es buena opcion SHA2
-texto_hashear_bytes=bytes("En KeepCoding aprendemos cómo protegernos con criptografía", "utf8") 
-m1.update(texto_hashear_bytes)
-print("sha512:    " + m1.digest().hex())
-
-
